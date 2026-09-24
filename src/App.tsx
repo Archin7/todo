8a654d6bfccbee434b01e799c84b0cdecb95841b
Archin7 <arch_in7@proton.me>
2026-09-24 19:59:32 +0100

    Initial thing

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./App.css";
+
+function TodoApp() {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<string[] | never[]>([]);
+  const todos_copy = todos.slice();
+
+  function handleClick() {
+    todos_copy.push(text);
+    setTodos(todos_copy);
+  }
+
+  const todos_list = todos.map((todo, index) => {
+    return (
+      <li key={index}>
+        <div className="todo">
+          <button
+            className="remove-todo"
+            onClick={() => setTodos(todos.filter((_, i) => i !== index))}
+          >
+            X
+          </button>{" "}
+          {todo}
+        </div>
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <div className="todos">
+        <ol>{todos_list}</ol>
+      </div>
+      <div className="add-todos">
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+        <button onClick={handleClick}>Add</button>
+      </div>
+    </>
+  );
+}
+
+export default TodoApp;
